99620fd38d1a80c066cb8ce9f8dc1f2f753a3444
Gabriele Fiorentino <gabrielefiorentino2@gmail.com>
2023-05-12 11:25:42 +0200

    Start to implement Magic Stones

---
+++ Assets/Scripts/6-Magic Stones/IMagicStone.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMagicStone
+{
+    void Activate();
+
+    void Deactivate();
+
+    void AddEffect();
+}
