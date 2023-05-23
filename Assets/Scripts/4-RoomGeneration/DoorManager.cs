dee605d78629d8a188cb27ae31f578a019ac8dd0
Gabriele Fiorentino <gabrielefiorentino2@gmail.com>
2023-05-23 18:43:52 +0200

    Door charging

--- Assets/Scripts/4-RoomGeneration/DoorManager.cs
+++ Assets/Scripts/4-RoomGeneration/DoorManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorManager : MonoBehaviour
 {
@@ -16,6 +17,12 @@ public class DoorManager : MonoBehaviour
     private Transform CameraPos;
     public bool enemySpawned;
 
+    [Header("Slider")] 
+    [SerializeField] private Slider doorSlider;
+    private float startValue;
+    private float targetValue = 1f;
+    private bool canGo = false;
+
     private void Start()
     {
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
@@ -23,62 +30,109 @@ public class DoorManager : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         roomData = currentRoom.GetComponent<RoomData>();
     }
-
-    private void OnCollisionEnter(Collision collision)
+    
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.CompareTag("Player") && enemySpawned == false)
+        if (other.gameObject.CompareTag("Player") && enemySpawned == false)
         {
-            var cameraPosition = mainCamera.transform.position;
-            switch (direction) {
-                case 1:
+            doorSlider.gameObject.SetActive(true);
+        
+            doorSlider.maxValue = 1f;
+            doorSlider.value = 0f;
+            startValue = doorSlider.value;
+            StartCoroutine(GoInOtherRoom());
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (canGo)
+        { 
+            if (other.gameObject.CompareTag("Player") && enemySpawned == false)
+            {
+                var cameraPosition = mainCamera.transform.position;
+                switch (direction) {
+                    case 1:
+                        
+                        cameraPosition = roomData.roomNord.GetComponentInChildren<SetCameraPos>().gameObject.transform.position;
+                        mainCamera.transform.position = cameraPosition;
+                        
+                        playerPos = player.transform.position;
+                        player.transform.position = new Vector3(playerPos.x,playerPos.y,playerPos.z + 25f);
+                        
+                        roomData.roomNord.SetActive(true);
+                        currentRoom.SetActive(false);
+                        break;
                     
-                    cameraPosition = roomData.roomNord.GetComponentInChildren<SetCameraPos>().gameObject.transform.position;
-                    mainCamera.transform.position = cameraPosition;
+                    case 2:
+                        
+                        cameraPosition = roomData.roomSouth.GetComponentInChildren<SetCameraPos>().gameObject.transform.position;
+                        mainCamera.transform.position = cameraPosition;
+                        
+                        playerPos = player.transform.position;
+                        player.transform.position = new Vector3(playerPos.x,playerPos.y,playerPos.z - 25f);
+                        
+                        roomData.roomSouth.SetActive(true);
+                        currentRoom.SetActive(false);
+                        break;
                     
-                    playerPos = player.transform.position;
-                    player.transform.position = new Vector3(playerPos.x,playerPos.y,playerPos.z + 25f);
+                    case 3:
+                        
+                        cameraPosition = roomData.roomEst.GetComponentInChildren<SetCameraPos>().gameObject.transform.position;
+                        mainCamera.transform.position = cameraPosition;
+                        
+                        playerPos = player.transform.position;
+                        player.transform.position = new Vector3(playerPos.x + 25f,playerPos.y,playerPos.z);
+                        
+                        roomData.roomEst.SetActive(true);
+                        currentRoom.SetActive(false);
+                        break;
                     
-                    roomData.roomNord.SetActive(true);
-                    currentRoom.SetActive(false);
-                    break;
-                
-                case 2:
+                    case 4:
+                        
+                        cameraPosition = roomData.roomWest.GetComponentInChildren<SetCameraPos>().gameObject.transform.position;
+                        mainCamera.transform.position = cameraPosition;
+                        
+                        playerPos = player.transform.position;
+                        player.transform.position = new Vector3(playerPos.x - 25f,playerPos.y,playerPos.z);
+                        
+                        roomData.roomWest.SetActive(true);
+                        currentRoom.SetActive(false);
+                        break;
                     
-                    cameraPosition = roomData.roomSouth.GetComponentInChildren<SetCameraPos>().gameObject.transform.position;
-                    mainCamera.transform.position = cameraPosition;
-                    
-                    playerPos = player.transform.position;
-                    player.transform.position = new Vector3(playerPos.x,playerPos.y,playerPos.z - 25f);
-                    
-                    roomData.roomSouth.SetActive(true);
-                    currentRoom.SetActive(false);
-                    break;
-                
-                case 3:
-                    
-                    cameraPosition = roomData.roomEst.GetComponentInChildren<SetCameraPos>().gameObject.transform.position;
-                    mainCamera.transform.position = cameraPosition;
-                    
-                    playerPos = player.transform.position;
-                    player.transform.position = new Vector3(playerPos.x + 25f,playerPos.y,playerPos.z);
-                    
-                    roomData.roomEst.SetActive(true);
-                    currentRoom.SetActive(false);
-                    break;
-                
-                case 4:
-                    
-                    cameraPosition = roomData.roomWest.GetComponentInChildren<SetCameraPos>().gameObject.transform.position;
-                    mainCamera.transform.position = cameraPosition;
-                    
-                    playerPos = player.transform.position;
-                    player.transform.position = new Vector3(playerPos.x - 25f,playerPos.y,playerPos.z);
-                    
-                    roomData.roomWest.SetActive(true);
-                    currentRoom.SetActive(false);
-                    break;
-                
+                }
             }
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && enemySpawned == false)
+        {
+            doorSlider.value = 0f;
+            startValue = doorSlider.value;
+            doorSlider.gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator GoInOtherRoom()
+    {
+        float duration = 1f;
+        float elapsedTime = 0f;
+
+        
+        
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / duration);
+            doorSlider.value = Mathf.Lerp(startValue, targetValue, t);
+
+
+            yield return null;
+        }
+        
+        canGo = true;
+        doorSlider.value = targetValue;
     }
 }
