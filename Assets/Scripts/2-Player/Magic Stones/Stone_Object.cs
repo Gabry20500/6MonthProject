e32f959a56b727eb4f8c608ad837d9dc5fd0de94
Gabriele Fiorentino <gabrielefiorentino2@gmail.com>
2023-06-13 17:09:44 +0200

    Stone gui update and start to populate tutorial

--- Assets/Scripts/2-Player/Magic Stones/Stone_Object.cs
+++ Assets/Scripts/2-Player/Magic Stones/Stone_Object.cs
@@ -1,10 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class Stone_Object : MonoBehaviour
 {
     [SerializeField] Stone stone;
+    
+    [Header("Levitation")]
+    [SerializeField] private float levitationDistance = 1f;
+    [SerializeField] private float levitationSpeed = 1f;
+    private Vector3 _originalPosition;
+
+    private void Start()
+    {
+        _originalPosition = transform.position; 
+    }
+
+    private void Update()
+    {
+        Vector3 targetPosition =
+            _originalPosition + Vector3.up * levitationDistance * Mathf.Sin(Time.time * levitationSpeed);
+
+        transform.position = targetPosition;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
--- Assets/Scripts/3-General/LevelManager.cs
+++ Assets/Scripts/3-General/LevelManager.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : Singleton<LevelManager>
 {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
 
     public int level = -1;
 
